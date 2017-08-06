92d1c5acd43cb07419fe2a4dfaef428bbc5a401b
tonyjaimep <tonyjaimep@gmail.com>
2017-08-06 18:00:25 -0500

    Now can make mistakes

--- typer.py
+++ typer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from selenium import webdriver
+from random import randint
 
 url = "http://keybr.com/practice"
 letter_selector = "span.TextInput-item"
@@ -8,6 +9,8 @@ focus_input_selector = ".TextInput.TextInput--sizeX0"
 
 # how many times to fill the field
 attempts = 10
+# percentage
+error_chance = 0
 
 # open browser
 browser = webdriver.Firefox()
@@ -26,6 +29,12 @@ for _ in range(attempts):
             continue
 
         value = letter.text if letter.text != "␣" else " "
-        letter.send_keys(value)
+        if (randint(1, 100) <= error_chance):
+            # send an incorrect key
+            letter.send_keys(chr(ord(value) + 1))
+            # then send the correct one
+            letter.send_keys(value)
+        else:
+            letter.send_keys(value)
 
 browser.close()
